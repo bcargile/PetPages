4ac12d061f6089af0e6166f928d9677a963ffc73
bjcargil <bjcargil@ncsu.edu>
2016-11-12 11:20:32 -0500

    Add project files.

---
+++ PetPages/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetPages.Startup))]
+namespace PetPages
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
